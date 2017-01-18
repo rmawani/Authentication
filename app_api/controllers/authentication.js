6aa6698f2d2d69a5f0a34ea49d438d633df6caab
rmawani <riz@digital-minds.ca>
2017-01-18 09:02:09 -0700

    Updated authentication.js

--- app_api/controllers/authentication.js
+++ app_api/controllers/authentication.js
@@ -67,7 +67,7 @@ module.exports.login = function(req, res) {
     } else {
       // If user is not found
       res.status(401).json(info);
-      console.log(err);
+      console.log(res.body);
     }
 
   })(req, res);
